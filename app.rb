3f1f1779dd28edd54ed83991afbdb35e6388ff76
Sekky <step1336@gmail.com>
2015-03-29 11:47:38 +0900

    second commit

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require './models/item.rb'
+
+set :bind, '192.168.33.10'
+set :port, 3000
+
+get '/' do
+	@items=Item.all
+	@total=Item.sum(:price)
+	@categories = Category.all
+  erb :index	
+end
+
+post '/create' do
+	Item.create({
+		title: params[:title],
+		price: params[:price],
+		category_id: params[:category]
+		})
+
+	redirect '/'
+end
+get'/edit/:id' do
+	@item= Item.find(params[:id])
+	erb :edit
+end
+
+get '/category/:id' do
+	@categories = Category.all
+	@category = Category.find(params[:id])
+	@items = @category.items
+	erb :index
+end
